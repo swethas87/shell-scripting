ac46f1dec88dc4bd3da93696efb1d389126575ea
Swetha <swetha.s87@gmail.com>
2024-04-17 03:27:30 +0100

    specialvars

--- 05-special_variables.sh
+++ 05-special_variables.sh
@@ -3,3 +3,10 @@
 echo "All variables: $@"
 echo "Numver of variables passed: $#"
 echo "Script name: $0"
+echo "print working directory: $PWD"
+echo "print home directory: $HOME"
+echo "whcih user: $USER"
+echo "Hostname: $HOSTNAME"
+echo "Process ID: $$"
+sleep 60 &
+echo "Process Id of last background commands: $!"
