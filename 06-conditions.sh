7e0d9b37bcd8ca6d0d94384f165ea35b056a7b7e
Swetha <swetha.s87@gmail.com>
2024-04-17 03:50:02 +0100

    conditions

--- 06-conditions.sh
+++ 06-conditions.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-NUMBER=$1
+NUMBER=$1gi
 
 if [$NUMBER -gt 10]
 then
     echo "Given number $NUMBER is greater than 10"
 else
     echo "Given number $NUMBER is less than 10"
-if
+fi
