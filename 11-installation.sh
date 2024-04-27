2012bf5d87501a789626285fb370c58de2a797c4
Swetha <swetha.s87@gmail.com>
2024-04-27 17:31:38 +0100

    test installation

--- 11-installation.sh
+++ 11-installation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 USERID=$(id -u)
-if [$USERID -ne 0]
+if [ $USERID -ne 0 ]
 then
-    echo " please run this script with root access"
+    echo "please run this script with root access"
 else
     echo "you are super user"
 fi
