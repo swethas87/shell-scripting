f16b6159c8667920552a650284066baf1f85524c
Swetha <swetha.s87@gmail.com>
2024-05-12 07:49:47 +0100

    calling other scripts

--- 21-currentscript.sh
+++ 21-currentscript.sh
@@ -5,7 +5,8 @@ VALUE="This is currentscript"
 echo "Before calling otherscript: $VALUE"
 echo "Process ID of current shell script: $$"
 
-./20-otherscript.sh
+#./20-otherscript.sh   #This way we are calling directly the script
+source ./20-otherscript.sh
 
 echo "After calling otherscript: $VALUE"
 echo "Process ID of current shell script: $$"
