f8679426ee72b13d6cd4ad05b3db4237e184309f
Swetha <swetha.s87@gmail.com>
2024-05-12 22:07:31 +0100

    set script testing

---
+++ 22-set.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+    echo "Please run this script with root access"
+    exit 1 #manually exit if error comes
+else
+    echo "You are super user"
+fi
+
+dnf install myshdhsql -y
+dnf install git -y
+
+echo"is script proceeding?"
